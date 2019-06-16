e619ada2f33b821cbc8c2e4991a8fe40c7b95438
Irtefa <mirtefa@gmail.com>
2019-06-15 21:20:49 -0500

    initial commit with 11 questions

---
+++ src/components/Home.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h1>{{ question }}</h1>
+    <p>Press any key for the next question</p>
+    <button v-on:click="next">
+      Next
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    question: String,
+  },
+
+  data: function() {
+    return {
+      questions: [
+        {title: 'When was the last time you entered the United States?'},
+        {title: 'When was the first time you entered the United States?'},
+        {title: "What's the name of your employer?"},
+        {title: "What's your day to day responsibility at your job?"},
+        {title: "What's your full name?"},
+        {title: "What's your father's full name?"},
+        {title: "What's your mother's full name?"},
+        {title: "What's the last physical address where you stayed at?"},
+        {title: "Are you married?"},
+        {title: "When did you join your current company?"},
+        {title: "Where did you live before your current physical address?"},
+      ],
+      counter: 0,
+    }
+  },
+
+  created: function() {
+    window.addEventListener('keyup', this.next)
+    this.questions = this.shuffle(this.questions)
+  },
+
+  methods: {
+    next: function() {
+      this.question = this.questions[this.counter].title  
+      this.counter += 1
+      if (this.counter === this.questions.length) {
+        this.counter = 0
+      }
+    },
+
+    onPan: function() {
+      this.next()
+    },
+
+    shuffle: function shuffle(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1)) // random index from 0 to i
+        [array[i], array[j]] = [array[j], array[i]] // swap elements
+      }
+      return array
+    }
+  }
+}
+</script>
